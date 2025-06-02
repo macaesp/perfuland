e914000a485a65e86f2b47af60e03db5a3bbdcef
macaesp <mac.espinoza@duocuc.cl>
2025-06-01 20:36:00 -0400

    commit

--- msvc-boleta/src/main/java/com/perfuland/msvc/boleta/service/BoletaServiceImpl.java
+++ msvc-boleta/src/main/java/com/perfuland/msvc/boleta/service/BoletaServiceImpl.java
@@ -1,26 +1,28 @@
 package com.perfuland.msvc.boleta.service;
 
+
 import com.perfuland.msvc.boleta.exceptions.BoletaException;
 import com.perfuland.msvc.boleta.model.Boleta;
+import com.perfuland.msvc.boleta.service.BoletaService;
 import com.perfuland.msvc.boleta.repository.BoletaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-
-
+import org.springframework.stereotype.Service;
 import java.util.List;
 
-public class BoletaServiceImpl {
+@Service
+public abstract class BoletaServiceImpl implements BoletaService {
     @Autowired
     private BoletaRepository boletaRepository;
 
     @Override
-    public List<Boleta> findAll () {
+    public List<Boleta> findAll(){
         return this.boletaRepository.findAll();
     }
 
     @Override
-    public Boleta findById(Long id) {
+    public Boleta findById(Long id){
         return this.boletaRepository.findById(id).orElseThrow(
-                () -> new BoletaException("La boleta con id"+id+" no se encuentra en la base de datos")
+                () -> new BoletaException("La boleta con id " + id + " no se encuentra en la base de datos")
         );
     }
 }
